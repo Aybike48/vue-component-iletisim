5c463c6e6a1c86ce77d2b7abf20953fd785e8313
Aybike48 <90464121+Aybike48@users.noreply.github.com>
2022-01-28 17:21:11 +0300

    Add files via upload

---
+++ vue-comp-iletisim/src/components/User.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <h1>Parent Component (User)</h1>
+    <p>Burası parent component yani herşeyin import edildiği component :)</p>
+    <button @click="changeName">adımı değiştir</button>
+    <p>childtan gelen değer: {{childData}}</p>
+    <hr>
+    <div class="row">
+      <app-user-detail @data="childData='Tunay'" :name="title"></app-user-detail>
+      <app-user-detail @data="childData=$event" :name="title"></app-user-detail>
+      <app-user-edit></app-user-edit>
+    </div>
+  </div>
+</template>
+<script>
+  import UserDetail from "./UserDetail";
+  import UserEdit from "./UserEdit";
+  export default {
+    components : {
+      appUserDetail : UserDetail,
+      appUserEdit : UserEdit,
+    },
+    data:function () {
+      return {
+        title : "Aybike",
+        childData : ""
+      }      
+    },
+    methods : {
+      changeName(){
+        this.title="Berkay"
+      },
+    
+    }
+  }
+</script>
+
+<style>
+  div.container{
+    margin-top: 30px;
+    padding: 20px 40px;
+    background-color: #6a8d99;
+    border: 1px solid #666;
+  }
+  div.row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
---
+++ vue-comp-iletisim/src/components/UserDetail.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wcol-md-6">
+    <h3>Child Component 1</h3>
+    <p>Ben User.vue isimli Parent Component'in içerisindeki bir Child componentim</p>
+    <p>kullanıcı adı: {{name}}</p>
+    <button @click="sendToParent">veriyi parente gönder</button>
+  </div>
+</template>
+<script>
+  export default{
+    props : ["name"],
+    methods :{
+        sendToParent(){
+        this.$emit("data","Defaulttan gelen");
+      },
+    }
+  }
+
+</script>
+<style scoped>
+  div {
+    background-color: lightcoral;
+    padding: 20px;
+    border: 1px solid #666;
+    display: inline-block;
+  }
+</style>
